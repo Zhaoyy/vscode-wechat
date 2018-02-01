c1024f772743789ab69628fd508fb95e685671de
mdanonymous <zjlfdx2005@gmail.com>
2018-02-01 11:30:44 +0800

    修复无法预览wxss、提示no provider的问题

--- src/wechatApp.ts
+++ src/wechatApp.ts
@@ -4,20 +4,14 @@ import * as path from "path";
 import * as child_process from "child_process";
 import * as fs from "mz/fs";
 import * as vscode from "vscode";
-import WxmlDocumentContentProvider from './WXMLDocumentContentProvider';
-
 
 import * as portServices from "./libs/port";
 
 const previewContentTpl = path.join(__dirname, "../../index.html");
 const previewContent = path.join(__dirname, "../../content.html");
 const previewUri = vscode.Uri.parse(`file://${previewContent}`);
+    // process.platform == "win32" ? `file:///${previewContent}`: previewContent);
 
-// const previewUri = vscode.Uri.parse(`file://authority///${previewContent}`);
-//vscode.Uri.parse(`HTMLPreview://authority${previewContent}`);
-//vscode.Uri.parse(process.platform == "win32" ? `file:///${previewContent}`: previewContent);
-// HTMLPreview://authority/preview
-const wxmlDocumentContentProvider = new WxmlDocumentContentProvider();
 
 export function startWechatAppServer(port): PromiseLike<any> {
     return new Promise((resolve, reject) => {
@@ -45,11 +39,8 @@ export function formatPreviewerContent(port) {
 
 export function createPreviewer() {
     const viewColumn = +vscode.window.activeTextEditor.viewColumn;
-    let registration = vscode.workspace.registerTextDocumentContentProvider('', wxmlDocumentContentProvider);
 
-    return vscode.commands.executeCommand('vscode.previewHtml', previewUri, Math.min(viewColumn + 1, vscode.ViewColumn.Three)).then(success=>{
-        console.log(success);
-    });
+    return vscode.commands.executeCommand('vscode.previewHtml', previewUri, Math.min(viewColumn + 1, vscode.ViewColumn.Three), "wechat App")
 }
 
 export function registerServer(port) {
@@ -57,7 +48,6 @@ export function registerServer(port) {
 }
 
 export function getRunningServer() {
-    console.log('getRunningServer , begin');
     return fs.readFile(path.join(__dirname, "../../port"))
         .then(data => {
             return portServices.isFreePort(data.toString())
@@ -71,9 +61,9 @@ export function getRunningServer() {
 }
 
 export function startPreviewWechatApp() {
+
     return getRunningServer()
         .then(running => {
-            console.log('startPreviewWechatApp, running = ', running);
             if (running) {
                 return createPreviewer();                    
             }
